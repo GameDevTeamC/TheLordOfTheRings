dbbf10ec4970dad6aca6db55e0c70b4daefb2dd3
Jo6o43 <joaoribeiro141516@gmail.com>
2023-11-13 11:25:59 +0000

    Librarys

---
+++ TheLordOfTheRings/text.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <locale.h>
+#include <string.h>
+
+
+void main() {
+	setlocale(LC_ALL, "Portuguese");
+
+
+
+}
