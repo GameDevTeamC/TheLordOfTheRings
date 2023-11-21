b589a32273abc66fd5eb8aeabced9a64d43c71fd
Jo6o43 <joaoribeiro141516@gmail.com>
2023-11-21 15:35:24 +0000

    Add //setlocale(LC_ALL, "Portuguese");

--- TheLordOfTheRings/text.c
+++ TheLordOfTheRings/text.c
@@ -124,6 +124,7 @@ void displayActions();
 
 int main() {
     menu();
+    setlocale(LC_ALL, "Portuguese");
 
     int currentPlayer = 1;
     int castarCoins[2] = { INITIAL_CASTAR_COINS, INITIAL_CASTAR_COINS };
